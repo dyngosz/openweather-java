64cc36b09672cb1286a85e7c757dba541ea16c09
dyngosz <jestemwiciu@gmail.com>
2017-04-06 20:06:14 +0200

    Weather icons displaying

--- src/main/resources/static/app/js/controllers.js
+++ src/main/resources/static/app/js/controllers.js
@@ -1,32 +1,430 @@
-(function (angular) {
+(function(angular) {
 
-    'use strict';
+	'use strict';
 
-    var App = angular.module('App');
+	var App = angular.module('App');
+	var weatherList = {
+		"200" : {
+			"label" : "thunderstorm with light rain",
+			"icon" : "storm-showers"
+		},
 
-    App.controller('WeatherController', ['$scope', '$http', '$routeParams', '$rootScope',
-        function ($scope, $http, $routeParams, $rootScope) {
+		"201" : {
+			"label" : "thunderstorm with rain",
+			"icon" : "storm-showers"
+		},
 
-            $scope.init = function () {
-                $rootScope.city = 'Wroclaw';
-                $scope.getWeather($rootScope.city);
-            };
-            
-            $scope.getWeather = function (city) {
+		"202" : {
+			"label" : "thunderstorm with heavy rain",
+			"icon" : "storm-showers"
+		},
 
-                $http.get('/weather', {
-                    params: {city: encodeURI(city)}
-                }).then(function (response) {
-                    $scope.showError = false;
-                    $scope.weather = response.data;
-                    $scope.weatherAvailable = true;
-                }, function (response) {
-                    $scope.showError = true;
-                })
-            };
+		"210" : {
+			"label" : "light thunderstorm",
+			"icon" : "storm-showers"
+		},
 
-            $scope.init();
+		"211" : {
+			"label" : "thunderstorm",
+			"icon" : "thunderstorm"
+		},
 
-        }]);
+		"212" : {
+			"label" : "heavy thunderstorm",
+			"icon" : "thunderstorm"
+		},
+
+		"221" : {
+			"label" : "ragged thunderstorm",
+			"icon" : "thunderstorm"
+		},
+
+		"230" : {
+			"label" : "thunderstorm with light drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"231" : {
+			"label" : "thunderstorm with drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"232" : {
+			"label" : "thunderstorm with heavy drizzle",
+			"icon" : "storm-showers"
+		},
+
+		"300" : {
+			"label" : "light intensity drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"301" : {
+			"label" : "drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"302" : {
+			"label" : "heavy intensity drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"310" : {
+			"label" : "light intensity drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"311" : {
+			"label" : "drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"312" : {
+			"label" : "heavy intensity drizzle rain",
+			"icon" : "sprinkle"
+		},
+
+		"313" : {
+			"label" : "shower rain and drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"314" : {
+			"label" : "heavy shower rain and drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"321" : {
+			"label" : "shower drizzle",
+			"icon" : "sprinkle"
+		},
+
+		"500" : {
+			"label" : "light rain",
+			"icon" : "rain"
+		},
+
+		"501" : {
+			"label" : "moderate rain",
+			"icon" : "rain"
+		},
+
+		"502" : {
+			"label" : "heavy intensity rain",
+			"icon" : "rain"
+		},
+
+		"503" : {
+			"label" : "very heavy rain",
+			"icon" : "rain"
+		},
+
+		"504" : {
+			"label" : "extreme rain",
+			"icon" : "rain"
+		},
+
+		"511" : {
+			"label" : "freezing rain",
+			"icon" : "rain-mix"
+		},
+
+		"520" : {
+			"label" : "light intensity shower rain",
+			"icon" : "showers"
+		},
+
+		"521" : {
+			"label" : "shower rain",
+			"icon" : "showers"
+		},
+
+		"522" : {
+			"label" : "heavy intensity shower rain",
+			"icon" : "showers"
+		},
+
+		"531" : {
+			"label" : "ragged shower rain",
+			"icon" : "showers"
+		},
+
+		"600" : {
+			"label" : "light snow",
+			"icon" : "snow"
+		},
+
+		"601" : {
+			"label" : "snow",
+			"icon" : "snow"
+		},
+
+		"602" : {
+			"label" : "heavy snow",
+			"icon" : "snow"
+		},
+
+		"611" : {
+			"label" : "sleet",
+			"icon" : "sleet"
+		},
+
+		"612" : {
+			"label" : "shower sleet",
+			"icon" : "sleet"
+		},
+
+		"615" : {
+			"label" : "light rain and snow",
+			"icon" : "rain-mix"
+		},
+
+		"616" : {
+			"label" : "rain and snow",
+			"icon" : "rain-mix"
+		},
+
+		"620" : {
+			"label" : "light shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"621" : {
+			"label" : "shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"622" : {
+			"label" : "heavy shower snow",
+			"icon" : "rain-mix"
+		},
+
+		"701" : {
+			"label" : "mist",
+			"icon" : "sprinkle"
+		},
+
+		"711" : {
+			"label" : "smoke",
+			"icon" : "smoke"
+		},
+
+		"721" : {
+			"label" : "haze",
+			"icon" : "day-haze"
+		},
+
+		"731" : {
+			"label" : "sand, dust whirls",
+			"icon" : "cloudy-gusts"
+		},
+
+		"741" : {
+			"label" : "fog",
+			"icon" : "fog"
+		},
+
+		"751" : {
+			"label" : "sand",
+			"icon" : "cloudy-gusts"
+		},
+
+		"761" : {
+			"label" : "dust",
+			"icon" : "dust"
+		},
+
+		"762" : {
+			"label" : "volcanic ash",
+			"icon" : "smog"
+		},
+
+		"771" : {
+			"label" : "squalls",
+			"icon" : "day-windy"
+		},
+
+		"781" : {
+			"label" : "tornado",
+			"icon" : "tornado"
+		},
+
+		"800" : {
+			"label" : "clear sky",
+			"icon" : "sunny"
+		},
+
+		"801" : {
+			"label" : "few clouds",
+			"icon" : "cloudy"
+		},
+
+		"802" : {
+			"label" : "scattered clouds",
+			"icon" : "cloudy"
+		},
+
+		"803" : {
+			"label" : "broken clouds",
+			"icon" : "cloudy"
+		},
+
+		"804" : {
+			"label" : "overcast clouds",
+			"icon" : "cloudy"
+		},
+
+		"900" : {
+			"label" : "tornado",
+			"icon" : "tornado"
+		},
+
+		"901" : {
+			"label" : "tropical storm",
+			"icon" : "hurricane"
+		},
+
+		"902" : {
+			"label" : "hurricane",
+			"icon" : "hurricane"
+		},
+
+		"903" : {
+			"label" : "cold",
+			"icon" : "snowflake-cold"
+		},
+
+		"904" : {
+			"label" : "hot",
+			"icon" : "hot"
+		},
+
+		"905" : {
+			"label" : "windy",
+			"icon" : "windy"
+		},
+
+		"906" : {
+			"label" : "hail",
+			"icon" : "hail"
+		},
+
+		"951" : {
+			"label" : "calm",
+			"icon" : "sunny"
+		},
+
+		"952" : {
+			"label" : "light breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"953" : {
+			"label" : "gentle breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"954" : {
+			"label" : "moderate breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"955" : {
+			"label" : "fresh breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"956" : {
+			"label" : "strong breeze",
+			"icon" : "cloudy-gusts"
+		},
+
+		"957" : {
+			"label" : "high wind, near gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"958" : {
+			"label" : "gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"959" : {
+			"label" : "severe gale",
+			"icon" : "cloudy-gusts"
+		},
+
+		"960" : {
+			"label" : "storm",
+			"icon" : "thunderstorm"
+		},
+
+		"961" : {
+			"label" : "violent storm",
+			"icon" : "thunderstorm"
+		},
+
+		"962" : {
+			"label" : "hurricane",
+			"icon" : "cloudy-gusts"
+		}
+	};
+
+	App.controller('WeatherController', [
+			'$scope',
+			'$http',
+			'$routeParams',
+			'$rootScope',
+			function($scope, $http, $routeParams, $rootScope) {
+
+				$scope.init = function() {
+					$rootScope.city = 'Wroclaw';
+					$scope.getWeather($rootScope.city);
+				};
+
+				$scope.getWeather = function(city) {
+
+					$http.get('/weather', {
+						params : {
+							city : encodeURI(city)
+						}
+					}).then(
+							function(response) {
+								$scope.showError = false;
+								$scope.weather = response.data;
+								$scope.weatherAvailable = true;
+
+								var prefix = 'wi wi-';
+								var code = angular
+										.fromJson(response.data.weatherID)
+
+								var today = new Date();
+								var hour = today.getHours();
+								var dayCycle = "";
+
+								if (hour > 6 && hour < 22) {
+									// Day time
+									dayCycle = "day-";
+
+								} else {
+									// Night time
+									dayCycle = "night-";
+								}
+
+								var icon = weatherList[code].icon;
+								icon = prefix + dayCycle + icon;
+								$scope.weatherIcon = icon;
+
+								$scope.value = weatherList[code].icon;
+								$scope.code = angular
+										.fromJson(response.data.weatherID);
+							}, function(response) {
+								$scope.showError = true;
+							})
+				};
+
+				$scope.init();
+
+			} ]);
 
 })(window.angular);
